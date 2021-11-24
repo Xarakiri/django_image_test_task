96de3913fcb6c1555d57e0dbb4ab81f500ec1a80
Xarakiri <rafikov.ds777@gmail.com>
2021-11-24 22:12:42 +0500

    add registration endpoint

--- api/routes.py
+++ api/routes.py
@@ -2,6 +2,7 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from .views import RegistrationAPIView
 
 
 schema_view = get_schema_view(
@@ -18,4 +19,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('users/', RegistrationAPIView.as_view()),
 ]
