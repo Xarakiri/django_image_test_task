aa42faa22f16065e6230d379b523e83cbea8aec3
Xarakiri <rafikov.ds777@gmail.com>
2021-11-24 22:23:03 +0500

    add log in endpoint

--- api/routes.py
+++ api/routes.py
@@ -2,7 +2,7 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, LoginAPIView
 
 
 schema_view = get_schema_view(
@@ -20,4 +20,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('users/', RegistrationAPIView.as_view()),
+    path('users/login/', LoginAPIView.as_view()),
 ]
